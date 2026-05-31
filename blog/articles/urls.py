167e4d2e90dc1d5101dfdd29f6450fd2044eb43a
eddieChadwick07 <eddie.chadwick0707@outlook.com>
2026-05-31 18:57:59 +0100

    Update to allow creation and edit of blogs from within the site with user validation.

--- blog/articles/urls.py
+++ blog/articles/urls.py
@@ -4,6 +4,10 @@ from .import views
 urlpatterns = [
     path('', views.ArticleListView.as_view(),
          name='home'),
-    path('article/<int:pk>', views.ArticleDetailView.as_view(),
-         name='detail'),
+    path('article/<int:pk>/', views.ArticleDetailView.as_view(),
+         name='article_detail'),
+    path('article/new/', views.ArticleNewView.as_view(),
+         name='article_new'),
+    path('article/edit/<int:pk>', views.ArticleEditView.as_view(),
+         name='article_edit'),
 ]
--- blog/articles/views.py
+++ blog/articles/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy, reverse
+
 from .models import Article
 
 class ArticleListView(ListView):
@@ -9,3 +12,27 @@ class ArticleListView(ListView):
 class ArticleDetailView(DetailView):
     model = Article
     template_name = 'detail.html'
+
+class ArticleNewView(CreateView, LoginRequiredMixin):
+    model = Article
+    template_name = 'new.html'
+    fields = ['title', 'body']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    success_url = reverse_lazy('home')
+
+
+class ArticleEditView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Article
+    template_name = 'edit.html'
+    fields = ['title', 'body']
+
+    def test_func(self):
+        article = self.get_object()
+        return article.author == self.request.user
+
+    def get_success_url(self):
+        return reverse('article_detail', kwargs={'pk': self.object.pk})
